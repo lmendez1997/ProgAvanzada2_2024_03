4eaa144b71c6034a1705b97075fbf38765a7dbf0
kcooperhn <134466354+kcooperhn@users.noreply.github.com>
2024-10-25 21:17:01 -0600

    Agregando proyecto vaadin
    sistema de matricula vaadin

---
+++ aplicacion-de-prueba/src/main/java/com/uth/hn/Application.java
@@ -0,0 +1,24 @@
+package com.uth.hn;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/dosis", version = "4.5.0")
+@Theme(value = "sistema-empleados")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ aplicacion-de-prueba/src/main/java/com/uth/hn/views/MainLayout.java
@@ -0,0 +1,81 @@
+package com.uth.hn.views;
+
+import com.uth.hn.views.carritodecompras.CarritodeComprasView;
+import com.uth.hn.views.consultar.ConsultarView;
+import com.uth.hn.views.listadodeempleados.ListadodeEmpleadosView;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+import org.vaadin.lineawesome.LineAwesomeIcon;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+    private H1 viewTitle;
+
+    public MainLayout() {
+        setPrimarySection(Section.DRAWER);
+        addDrawerContent();
+        addHeaderContent();
+    }
+
+    private void addHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.setAriaLabel("Menu toggle");
+
+        viewTitle = new H1();
+        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+
+        addToNavbar(true, toggle, viewTitle);
+    }
+
+    private void addDrawerContent() {
+        Span appName = new Span("Aplicacion de prueba");
+        appName.addClassNames(LumoUtility.FontWeight.SEMIBOLD, LumoUtility.FontSize.LARGE);
+        Header header = new Header(appName);
+
+        Scroller scroller = new Scroller(createNavigation());
+
+        addToDrawer(header, scroller, createFooter());
+    }
+
+    private SideNav createNavigation() {
+        SideNav nav = new SideNav();
+
+        nav.addItem(new SideNavItem("Carrito de Compras", CarritodeComprasView.class,
+                LineAwesomeIcon.SHOPPING_BASKET_SOLID.create()));
+        nav.addItem(
+                new SideNavItem("Consultar", ConsultarView.class, LineAwesomeIcon.ACQUISITIONS_INCORPORATED.create()));
+        nav.addItem(new SideNavItem("Listado de Empleados", ListadodeEmpleadosView.class,
+                LineAwesomeIcon.LIST_SOLID.create()));
+
+        return nav;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+
+        return layout;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
